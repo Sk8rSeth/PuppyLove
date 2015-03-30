9b94ebd15c7db6624be95f4bbb9db723bcad1113
Sk8rSeth <sethhowell2197@gmail.com>
2015-03-30 13:36:37 -0700

    init

---
+++ RawIndex.php
@@ -0,0 +1,132 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>CSS Site</title>
+	<link rel="stylesheet" href="http://necolas.github.io/normalize.css/3.0.2/normalize.css">
+	<link rel="stylesheet" href="RawStyles.css">
+	<link href='http://fonts.googleapis.com/css?family=Lato:300,700|Lobster' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<div class="hero">
+		<div class="header-container">
+			<div class="logo">
+				<a href="/CSS/">Puppy Love</a>
+			</div>
+			<div class="searchbar">
+				<form action="">
+					<input type="text">
+					<button>Search	</button>
+				</form>
+			</div>
+			<nav class="navbar">
+				<span>HOME</span>
+				<span>SERVICES</span>
+				<span>PORTFOLIO</span>
+				<span>ABOUT</span>
+				<span>MEMBERSHIP</span>
+				<span>TESTOMONIALS</span>
+				<span>FAQ</span>
+				<span>CONTACT</span>
+			</nav>
+		</div>
+		<div class="blankspace">
+			<div>
+				<h1>Good <span>People</span> Great <span>Dogs</span></h1>
+				<p>Welcome to the best site on the iternet! We really hope you buy our products because we really love to make money!</p>
+			</div>
+		</div>
+	</div>
+	<main class="content">
+		<div class="triple-container">
+			<div>
+				<h3>raise a litter</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat suscipit, assumenda ratione tenetur corporis.</p>
+				<button>get help</button>
+			</div>
+			<div>
+				<h3>feed or somethin</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores laboriosam repellendus rem aliquid laborum.</p>
+				<button>get food</button>
+			</div>
+			<div>
+				<h3>that third thing</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil laborum incidunt accusantium quisquam maiores.</p>
+				<button>give em to china</button>
+			</div>
+		</div>
+		<div class="left-side">
+			<div class='aside'>
+				<div class="media-object-container">
+					<div class="image">
+						<img src="/css/Images/dog1.png">
+					</div>
+					<div class="comment">
+						<div>Ralf</div>
+						<div>Lorem ipsum dolor sit amet.</div>
+					</div>
+				</div>
+				<div class="media-object-container">
+					<div class="image">
+						<img src="/css/Images/dog2.png">
+					</div>
+					<div class="comment">
+						<div>Max</div>
+						<div>Lorem ipsum dolor sit amet.</div>
+					</div>
+				</div>
+				<div class="media-object-container">
+					<div class="image">
+						<img src="/css/Images/dog3.png">
+					</div>
+					<div class="comment">
+						<div>Rudy</div>
+						<div>Lorem ipsum dolor sit amet.</div>
+					</div>
+				</div>
+			</div>
+			<div class="gallery">
+				<div class="tile">
+					<img src="/css/Images/dog1.png">
+					<div>Ralf</div>
+					<span>This old mutt likes to roll in the dirt.</span>
+				</div>
+				<div class="tile">
+					<img src="/css/Images/dog2.png">
+					<div>Max</div>
+					<span>if there was ever a young mutt that liked grass, it's Max</span>
+					
+				</div>
+				<div class="tile">
+					<img src="/css/Images/dog3.png">
+					<div>Rudy</div>
+					<span>Young and fierce, thats all I have to say</span>
+					
+				</div>
+				<div class="tile">
+					<img src="/css/Images/dog3.png">
+					<div>Rudy</div>
+					<span>Young and fierce, thats all I have to say</span>
+					
+				</div>
+				<div class="tile">
+					<img src="/css/Images/dog2.png">
+					<div>Max</div>
+					<span>if there was ever a young mutt that liked grass, it's Max</span>
+					
+				</div>
+				<div class="tile">
+					<img src="/css/Images/dog1.png">
+					<div>Ralf</div>
+					<span>This old mutt likes to roll in the dirt.</span>
+					
+				</div>
+			</div>
+			<div></div>
+			<div></div>
+		</div>
+	</main>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,127 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>CSS Site</title>
+	<link rel="stylesheet" href="http://necolas.github.io/normalize.css/3.0.2/normalize.css">
+	<link rel="stylesheet" href="styles.css">
+	<link href='http://fonts.googleapis.com/css?family=Lato:300,700|Lobster' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<div class="hero">
+		<header>
+			<a href="/CSS/" class="logo">Puppy Love</a>
+			<form action="/" class="searchbar">
+				<input type="text" placeholder="Search">
+				<button>Search</button>
+			</form>
+			<nav>
+				<a href="/css">Home</a>
+				<a href="/css">Services</a>
+				<a href="/css">Portfolio</a>
+				<a href="/css">About</a>
+				<a href="/css">Membership</a>
+				<a href="/css">Testomonials</a>
+				<a href="/css">FAQ</a>
+				<a href="/css">Contact</a>
+			</nav>
+		</header>
+		<div class="blankspace">
+			<div>
+				<h1>Good <span>People</span> Great <span>Dogs</span></h1>
+				<p>Welcome to the best site on the iternet! We really hope you buy our products because we really love to make money!</p>
+			</div>
+		</div>
+	</div>
+	<main>
+		<div class="breakouts">
+			<div>
+				<h3>Raise A Litter</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat suscipit, assumenda ratione tenetur corporis.</p>
+				<button>Get Help</button>
+			</div>
+			<div>
+				<h3>Feed 'em Right</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores laboriosam repellendus rem aliquid laborum.</p>
+				<button>Get Food</button>
+			</div>
+			<div>
+				<h3>Sell Your Pups</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil laborum incidunt accusantium quisquam maiores.</p>
+				<button>Sell Dogs To China</button>
+			</div>
+		</div>
+		<div class="main-content">
+			<aside>
+				<div class="media-object">
+					<div>
+						<img src="/css/Images/dog1.png">
+					</div>
+					<div class="comment">
+						<div>Ralf</div>
+						<div>This old mutt likes to roll in the dirt.</div>
+					</div>
+				</div>
+				<div class="media-object">
+					<div>
+						<img src="/css/Images/dog2.png">
+					</div>
+					<div class="comment">
+						<div>Max</div>
+						<div>if there was ever a young mutt that liked grass, it's Max</div>
+					</div>
+				</div>
+				<div class="media-object">
+					<div>
+						<img src="/css/Images/dog3.png">
+					</div>
+					<div class="comment">
+						<div>Rudy</div>
+						<div>Young and fierce, thats all I have to say</div>
+					</div>
+				</div>
+			</aside>
+			<div class="gallery">
+				<div class="tile">
+					<img src="/css/Images/dog1.png">
+					<div>Ralf</div>
+					<span>This old mutt likes to roll in the dirt.</span>
+				</div>
+				<div class="tile">
+					<img src="/css/Images/dog2.png">
+					<div>Max</div>
+					<span>if there was ever a young mutt that liked grass, it's Max</span>
+					
+				</div>
+				<div class="tile">
+					<img src="/css/Images/dog3.png">
+					<div>Rudy</div>
+					<span>Young and fierce, thats all I have to say</span>
+					
+				</div>
+				<div class="tile">
+					<img src="/css/Images/dog3.png">
+					<div>Rudy</div>
+					<span>Young and fierce, thats all I have to say</span>
+					
+				</div>
+				<div class="tile">
+					<img src="/css/Images/dog2.png">
+					<div>Max</div>
+					<span>if there was ever a young mutt that liked grass, it's Max</span>
+					
+				</div>
+				<div class="tile">
+					<img src="/css/Images/dog1.png">
+					<div>Ralf</div>
+					<span>This old mutt likes to roll in the dirt.</span>
+					
+				</div>
+			</div>
+		</div>
+	<div>&copy; 2015</div>
+	</main>
+</body>
+</html>
